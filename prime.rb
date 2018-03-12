d88b001a70dbd7d4a87e7c781a5bdadd7821a2d0
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-12 23:54:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(integer)
+  array = (2..integer)
+  if integer >1
+  array.any? do |number|
+     integer % number == 0
+  end
+else
+  false
+end
+
+#array = (-1..Math.sqrt(integer)).to_a
