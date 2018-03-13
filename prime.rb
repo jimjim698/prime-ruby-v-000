3482f6671fd3f61d07c41bb769d7bd7381b95dba
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-13 01:12:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(integer)
   return false if integer < 2
 array = (2..integer-1).to_a
   array.each do |number|
-if  (integer % number) == 0
+if  integer % number == 0
   return false
     end
   end
