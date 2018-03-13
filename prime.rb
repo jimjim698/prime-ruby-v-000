ff7de99f4af18b2deb779854763081513416e914
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-13 00:10:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,16 @@
 # Add  code here!
 
-#def prime?(integer)
-#  array = (2..integer)
-#  if integer >1
-#  array.each do |number|
-#  return true if   integer % number == 0
-#end
-#false
-#else
-#  false
-#end
-#end
+def prime?(integer)
+  array = (2..integer)
+  if integer >1
+  array.each do |number|
+  return false if   integer % number == 0
+end
+true
+else
+  false
+end
+end
 #array = (-1..Math.sqrt(integer)).to_a
 
-def prime?(num)
-        (2..num).each { |i| return false if num % i == 0}
-        true
-    end
+
