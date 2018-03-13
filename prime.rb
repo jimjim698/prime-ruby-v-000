9824af2a1345178fbbdc30962360e56aad9ca316
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-13 00:05:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(integer)
   array = (2..integer)
   if integer >1
   array.each do |number|
-  return false if   integer % number == 0
+  return true if   integer % number == 0
 end
-true
+false
 else
   false
 end
